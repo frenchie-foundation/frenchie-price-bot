9f0348eea715fc7eb309de087c6b2eeb441ebf38
Davi Rodrigues <developer.davi@gmail.com>
2021-04-19 22:29:37 -0300

    feat: add new member handler

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
     ecmaVersion: 12,
   },
   rules: {
-    indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
